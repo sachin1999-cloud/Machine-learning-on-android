24f262f262da7655a17625a87e7c01922d20364d
Sachin Sharma <63642280+sachin1999-cloud@users.noreply.github.com>
2021-05-14 21:16:19 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.ml1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.example.ml1.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private Button select, predict;
+    private TextView tv;
+    private Bitmap img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = (ImageView) findViewById(R.id.imageView3);
+        tv = (TextView) findViewById(R.id.textView4);
+        select = (Button) findViewById(R.id.button7);
+        predict = (Button) findViewById(R.id.button8);
+
+
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 100);
+
+            }
+        });
+
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                img = Bitmap.createScaledBitmap(img, 150, 150, true);
+
+                try {
+
+                    Model model = Model.newInstance(getApplicationContext());
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 150, 150, 3}, DataType.FLOAT32);
+                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    tensorImage.load(img);
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets result.
+                    Model.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    model.close();
+                    tv.setText(outputFeature0.getFloatArray()[0] + "\n"+outputFeature0.getFloatArray()[1]);
+
+
+                } catch (IOException e) {
+                    // TODO Handle the exception
+
+
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100)
+        {
+            imgView.setImageURI(data.getData());
+
+            Uri uri = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
